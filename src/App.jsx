8f224a69ee8965b9c60683f06680b141260ad277
iamebenezer <abegundeebenezer2004@gmail.com>
2024-02-16 16:41:02 +0100

    ds

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import './App.css'
 import { CiMail } from "react-icons/ci";
 import img from '../src/assets/dp.jpg'
 import { FaGithub } from "react-icons/fa";
+import { PiVideoBold } from "react-icons/pi";
 import { useTypewriter, Cursor } from 'react-simple-typewriter';
-
+import { FaLaptopCode } from "react-icons/fa6";
+import { MdDesignServices } from "react-icons/md";
 function App() {
   const [typeEffect]= useTypewriter({
     words:["Hey There, I'm Eben"],
@@ -36,13 +38,14 @@ function App() {
         <div className="flex flex-row justify-between  gap-10">
       <div className="flex flex-col justify-start items-start space-y-32">
       <div>
-      <h1 className='font-head  md:text-[50px]'>
-         Hey There,  <br/> I'm Eben</h1>
+      <h1 className='font-head md:text-[60px] text-teal-950'>
+      
+      {typeEffect}</h1>
       </div>
       <div className="font-text font-bold text-green-900 tracking-wider">
         <span>abegundeebenezer2004@gmail.com</span>
       </div>
-      <div className="flex flex-row items-center justify-center">
+      <div className="flex flex-row items-center justify-center ">
         <span className='font-head md:text-4xl text-green-900'>3</span>
         <span className='font-text md:text-xl font-semibold'>Years Experience</span>
       </div>
@@ -87,57 +90,87 @@ function App() {
    {/* What i do */}
 
         <section className="md:mx-12 pt-2 flex justify-center mx-6 mb-8">
-          <div className="grid md:grid-cols-2 grid-cols-1 place-content-center place-items-center  ">
-          <div className='flex md:hidden flex-col'>
-              <h2 className=' md:text-[40px] font-head text-teal-950 text-4xl text-center'>What do i help?</h2>
-              <p className=' text-teal-900 text-center'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste a quis fugiat?
+          <div className="grid md:grid-cols-2 grid-cols-1   lg:px-32"> 
+          <div className='flex md:hidden lg:hidden flex-col'>
+              <h2 className=' md:text-[40px] font-head text-teal-950 text-[50px] text-center'>What do i help?</h2>
+              <p className=' text-teal-900 text-center text-2xl'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste a quis fugiat?
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo debitis sed porro!
 
               </p>
+              <div className="grid grid-cols-2 place-items-center mt-4 gap-4">
+                <div className="">
+                <h3 className='font-head text-[45px]'>30+</h3>
+                <p className='font-text text-xl whitespace-nowrap'>Project Completed</p>
+                </div>
+                <div className="">
+                <h3 className='font-head text-[45px]'>50+</h3>
+                <p className='font-text text-xl whitespace-nowrap'>Project Completed</p>
+                </div>
+                
+
+                
+              </div>
               </div>
             <div>
             
           <div className="grid grid-row-3 gap-10 pt-4">
                 <div className="bg-white md:w-72 w-96 text-center h-fit shadow-lg shadow-gray-400 rounded-xl">
                       <div className="flex flex-row justify-center items-start p-8 space-x-4">
-                      <div className='bg-yellow-400 p-2 rounded-3xl'>
-                      <FaGithub size={40} />
+                      <div className='bg-yellow-400 p-3 rounded-full'>
+                      <FaLaptopCode size={40} />
                       </div>
                       <div className="">
-                        <h3 className='font-head text-teal-950 md:text-[20px]'>Website Design</h3>
+                        <h3 className='font-head text-teal-950 md:text-[20px] whitespace-nowrap'>Website Design</h3>
                         <p className="font-text text-teal-900 md:text-[15px]">  50+ Projects</p>
                       </div>
                       </div>
                 </div>
                 <div className="bg-white md:w-72 h-fit w-96 shadow-lg shadow-gray-400 rounded-xl">
                       <div className="flex flex-row justify-center items-start p-8 space-x-4">
-                        <div className='bg-red-400 p-2 rounded-3xl'>
-                      <FaGithub size={40} />
+                        <div className='bg-red-400 p-3 rounded-full'>
+                        <MdDesignServices size={40} />
                       </div>
                       <div className="">
-                        <h3 className='font-head text-teal-950 md:text-[20px]'>Website Design</h3>
-                        <p className="font-text text-teal-900 md:text-[15px]">  50+ Projects</p>
+                        <h3 className='font-head text-teal-950 md:text-[20px] whitespace-nowrap'>Graphic Deisgn</h3>
+                        <p className="font-text text-teal-900 md:text-[15px]">  60+ Designs</p>
                       </div>
                       </div>
                 </div>
                 <div className="bg-white md:w-72 w-96 h-fit shadow-lg shadow-gray-400 rounded-xl">
                       <div className="flex flex-row justify-center items-start p-8 space-x-4">
-                      <FaGithub size={40} />
+                      <div className='bg-orange-400 p-3 rounded-full'>
+                        <PiVideoBold size={40} />
+                      </div>
                       <div className="">
-                        <h3 className='font-head text-teal-950 md:text-[20px]'>Website Design</h3>
-                        <p className="font-text text-teal-900 md:text-[15px]">  50+ Projects</p>
+                        <h3 className='font-head text-teal-950 md:text-[20px] whitespace-nowrap'>Video Editing</h3>
+                        <p className="font-text text-teal-900 md:text-[15px]">  30 Projects</p>
                       </div>
                       </div>
                 </div>
               </div>
               </div>
               {/* 2 */}
-              <div className='hidden'>
+              <div className='hidden md:flex flex-col items-start'>
               <h2 className='md:text-[40px] font-head text-teal-950'>What do i help?</h2>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste a quis fugiat?
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo debitis sed porro!
+              <p className='font-text py-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint in eum quaerat. 
 
               </p>
+              <p className='font-text py-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint in eum quaerat. 
+
+              </p>
+               <div className="grid grid-cols-2 place-items-center mt-4 gap-8">
+                <div className="">
+                <h3 className='font-head text-[45px]'>30+</h3>
+                <p className='font-text text-xl '>Project Completed</p>
+                </div>
+                <div className="">
+                <h3 className='font-head text-[45px]'>50+</h3>
+                <p className='font-text text-xl '>Project Completed</p>
+                </div>
+                
+
+                
+              </div>
               </div>
           </div>
         </section>
